bda5d910aff4057632a8bd95e6657dbf5365133d
CJ <cjfields24@yahoo.com>
2020-08-03 11:57:15 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect
 import csv
 
 app = Flask(__name__)
@@ -8,33 +8,28 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
 
+
 def write_to_file(data):
 	with open('database.txt', mode='a') as database:
 		name = data['name']
-		email = data['email'] 
+		email = data['email']
 		message = data['message']
 		file = database.write(f'\n{name},{email},{message}')
+
 
 def write_to_csv(data):
 	with open('database.csv', newline='', mode='a') as database2:
 		name = data['name']
-		email = data['email'] 
+		email = data['email']
 		message = data['message']
-		csv_writer = csv.writer(database2, delimiter=' ', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		csv_writer.writerow([name,email,message])
-
-
-
-
-
-
-
-
-
+		csv_writer = csv.writer(database2, delimiter=' ',
+		                        quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		csv_writer.writerow([name, email, message])
 # @app.route('/index.html')
 # def my_home():
 #     return render_template('index.html')
@@ -43,16 +38,14 @@ def write_to_csv(data):
 # @app.route('/contact.html')
 # def contact():
 #     return render_template('contact.html')
-
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
     	try:
-		data = request.form.to_dict()
-		write_to_csv(data)
-		return redirect('/thankyou.html')
-	except:
-		return 'did not save to DB'
+	    	data = request.form.to_dict()
+	    	write_to_csv(data)
+	    	return redirect('/thankyou.html')
+	    except:
+	    	return 'did not save to DB'
     else:
     	return 'Please Try Again!'
-
